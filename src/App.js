609ae636f9e66072a0bde04d3fce722b763ba849
BenilDavid <DoodleDavid>
2022-12-09 20:14:45 +0530

    update

--- src/App.js
+++ src/App.js
@@ -41,15 +41,15 @@ function App() {
               : portionCount === 2 ?
                 <>
                   <div className="type-container">
-                        <Typewriter
-                          options={{
-                            strings: ['Early Interactions in the waiting room will be Rewarded a POAP. Time is running out.'],
-                            autoStart: true,
-                            loop: true,
-                            delay: 40,
-                            pauseFor: 100000,
-                          }}
-                        />
+                    <Typewriter
+                      options={{
+                        strings: ['Early Interactions in the waiting room will be Rewarded a POAP. Time is running out.'],
+                        autoStart: true,
+                        loop: true,
+                        delay: 40,
+                        pauseFor: 100000,
+                      }}
+                    />
                   </div>
 
                   <a className="intent-tweet-link animate__animated animate__fadeInUp" href="https://twitter.com/intent/tweet?%0a&url=https%3A%2F%2Ftwitter.com%2Fshylocknft%2Fstatus%2F1600556103976488962%3Fs%3D46%26t%3DsVT79eisuh7ca-VdLLCnLA&%0a&via=shylocknft&%0a&text=I%27m%20here%20in%20Tokyo%20(%E6%9D%B1%E4%BA%AC)%2C%20psyched%20to%20meet%20the%20most%20famous%20Detective%20Shylock%2C%20and%20eager%20to%20solve%20cases%20with%20him.%20Not%20only%20that%20early%20entrants%20will%20receive%20a%20%28POAP%29%20Proof%20of%20Attendance%20Protocol%20%F0%9F%8E%AB%20soon.%0a&hashtags=SolvewithShylock">
@@ -60,14 +60,16 @@ function App() {
                 </>
                 : ""}
 
+            <div className='footer'>
+              <button className="twitter-btn">
+                <a href="https://twitter.com/shylocknft">
+                  <img src={twitterIcon} className="twitter-logo" alt="twitter" />
+                </a>
+              </button>
+            </div>
+
           </div>
-          <div className='footer'>
-                    <button className="twitter-btn animate__animated animate__fadeInUp">
-                      <a href="https://twitter.com/shylocknft">
-                        <img src={twitterIcon} className="twitter-logo" alt="twitter" />
-                      </a>
-                    </button>
-                  </div>
+
         </div>
       </header>
     </div>
