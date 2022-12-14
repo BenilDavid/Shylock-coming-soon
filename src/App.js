816449a4b85ce3f42f86fe049c72eccf969c3ce7
BenilDavid <DoodleDavid>
2022-12-14 16:56:15 +0530

    minor fixes

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
 
   // const [jasperAudio, setjasperAudio] = useState(false);
   const [typingAudio, setTypingAudio] = useState(false);
+  const [startType, setStartType] = useState(false);
   // const [bgmAudio, setBgmAudio] = useState(false);
   const [portionCount, setportionCount] = useState(0);
 
@@ -26,6 +27,7 @@ function App() {
 
   const TypingAudioReady = () => {
     setTypingAudio(true);
+    setStartType(true);
   } 
 
   // const VoiceReady = () => {
@@ -59,23 +61,26 @@ function App() {
               : portionCount === 2 ?
                 <>
                   <div className="type-container">
-                  <ReactPlayer className="d-none" url={Typing} playing={typingAudio} controls={true} volume={1} muted={false} loop={false} playbackRate={1.3} onReady={TypingAudioReady} />
+                  <ReactPlayer className="d-none" url={Typing} playing={typingAudio} controls={true} volume={1} muted={false} loop={true} playbackRate={1.3} onReady={TypingAudioReady} />
+
+                    {startType ?
                     <Typewriter
-                      // onInit={(typewriter) => {
-                      //   typewriter.typeString('Early Interactions in the waiting room will be rewarded a POAP. Time is running out.')
-                      //     .callFunction(() => {
-                      //       setTypingAudio(false);
-                      //     })
-                      //     .start();
-                      // }}
-                      options={{
-                        strings: ['Early Interactions in the waiting room will be rewarded a POAP. Time is running out.'],
-                        loop: false,
-                        autoStart: true,
-                        delay: 45,
-                        pauseFor: 100000,
-                      }}
-                    />
+                    onInit={(typewriter) => {
+                      typewriter.typeString('Early Interactions in the waiting room will be rewarded a POAP. Time is running out.')
+                        .callFunction(() => {
+                          setTypingAudio(false);
+                        })
+                        .start();
+                    }}
+                    options={{
+                      // strings: ['Early Interactions in the waiting room will be rewarded a POAP. Time is running out.'],
+                      loop: false,
+                      // autoStart: true,
+                      delay: 60,
+                      pauseFor: 100000,
+                    }}
+                  />
+                : ''}
                   </div>
 
                   <a className="intent-tweet-link animate__animated animate__fadeInUp" href="https://twitter.com/intent/tweet?%0a&url=https%3A%2F%2Ftwitter.com%2Fshylocknft%2Fstatus%2F1600556103976488962%3Fs%3D46%26t%3DsVT79eisuh7ca-VdLLCnLA&%0a&via=shylocknft&%0a&text=I%27m%20here%20in%20Tokyo%20(%E6%9D%B1%E4%BA%AC)%2C%20psyched%20to%20meet%20the%20most%20famous%20Detective%20Shylock%2C%20and%20eager%20to%20solve%20cases%20with%20him.%20Not%20only%20that%20early%20entrants%20will%20receive%20a%20%28POAP%29%20Proof%20of%20Attendance%20Protocol%20%F0%9F%8E%AB%20soon.%0a&hashtags=SolvewithShylock">
