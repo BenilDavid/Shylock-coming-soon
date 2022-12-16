cd020c353b453dc29b5b1a959b1086602e62010e
BenilDavid <DoodleDavid>
2022-12-16 14:56:36 +0530

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ReactPlayer from 'react-player';
 import Coming from './Audio/coming-soon.mp3';
 import Typing from './Audio/typeSound.mp3';
 import Bgm from './Audio/shylock-bgm.mp3';
-import JasperVid from './Audio/jasper.webm';
+import JasperVid from './Audio/jasper.mp4';
 import Typewriter from 'typewriter-effect';
 import 'animate.css';
 
@@ -90,7 +90,7 @@ function App() {
                       : ''}
                   </div>
 
-                  <a className="intent-tweet-link animate__animated animate__fadeInUp" href="https://twitter.com/intent/tweet?%0a&url=https%3A%2F%2Ftwitter.com%2Fshylocknft%2Fstatus%2F1600556103976488962%3Fs%3D46%26t%3DsVT79eisuh7ca-VdLLCnLA&%0a&via=shylocknft&%0a&text=I%27m%20here%20in%20Tokyo%20(%E6%9D%B1%E4%BA%AC)%2C%20psyched%20to%20meet%20the%20most%20famous%20Detective%20Shylock%2C%20and%20eager%20to%20solve%20cases%20with%20him.%20Not%20only%20that%20early%20entrants%20will%20receive%20a%20Entry%20access%20Proof%20of%20Attendance%20Protocol%20%F0%9F%8E%AB%20soon.%0a&hashtags=SolvewithShylock">
+                  <a className="intent-tweet-link animate__animated animate__fadeInUp" href="https://twitter.com/intent/tweet?%0a&url=https%3A%2F%2Ftwitter.com%2Fshylocknft%2Fstatus%2F1600556103976488962%3Fs%3D46%26t%3DsVT79eisuh7ca-VdLLCnLA&%0a&via=shylocknft&%0a&text=I%27m%20here%20in%20Tokyo%20(%E6%9D%B1%E4%BA%AC)%2C%20psyched%20to%20meet%20the%20most%20famous%20Detective%20Shylock%2C%20and%20eager%20to%20solve%20cases%20with%20him.%20Not%20only%20that%20early%20entrants%20will%20receive%20a%20Entry%20access.%0a&hashtags=SolvewithShylock">
                     <button className="intent-tweet">
                       WAITING ROOM
                     </button>
